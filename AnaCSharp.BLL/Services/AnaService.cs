152d441e48e78fe3532447bd0b8180068bb8f777
JulienBara <bara.julien@yahoo.fr>
2018-05-19 13:26:04 +0200

    prepare service

--- AnaCSharp.BLL/Services/AnaService.cs
+++ AnaCSharp.BLL/Services/AnaService.cs
@@ -1,16 +1,83 @@
-﻿using AnaCSharp.DAL;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using AnaCSharp.DAL;
+using AnaCSharp.DAL.Repositories;
 
 namespace AnaCSharp.BLL.Services
 {
     public class AnaService
     {
         private readonly AnaContext _anaContext;
+        private readonly DeterminedWordRepository _determinedWordRepository;
+        private readonly DeterminingStateRepository _determiningStateRepository;
+        private readonly LogWordRepository _logWordRepository;
 
-        public AnaService(AnaContext anaContext)
+        private readonly Dictionary<int, List<string>> _lastWordsDictionary = new Dictionary<int, List<string>>();
+
+        public AnaService(
+            AnaContext anaContext,
+            DeterminedWordRepository determinedWordRepository,
+            DeterminingStateRepository determiningStateRepository,
+            LogWordRepository logWordRepository)
         {
             _anaContext = anaContext;
+            _determinedWordRepository = determinedWordRepository;
+            _determiningStateRepository = determiningStateRepository;
+            _logWordRepository = logWordRepository;
         }
 
-        
+        //public string AnalyzeLastChatMessage(string message, int chatId)
+        //{
+        //    message = message + " EOM";
+
+        //}
+
+        public void LogMessages(string message, int chatId)
+        {
+            var words = message.Split();
+            foreach (var word in words)
+            {
+                _logWordRepository.SaveLogWord(chatId, word);
+            }
+        }
+
+
+        public List<string> GetLastWordsOfChat(int chatId)
+        {
+            if (!_lastWordsDictionary.ContainsKey(chatId))
+            {
+                _lastWordsDictionary.Add(chatId, new List<string>());
+
+            }
+            return _lastWordsDictionary[chatId];
+        }
+
+        public void InsertNewWordInList(ref List<string> list, string word, int markovDegree)
+        {
+            list.Add(word);
+            if (list.Count > markovDegree)
+            {
+                list.RemoveAt(0);
+            }
+        }
+
+        public void Learn(string message, int chatId, ref List<string> lastWords, int markovDegree)
+        {
+            var words = message.Split();
+            foreach (var word in words)
+            {
+                LearnAState(word, lastWords, markovDegree);
+                InsertNewWordInList(ref lastWords, word, markovDegree);
+            }
+        }
+
+        public void LearnAState(string word, List<string> lastWords, int markovDegree)
+        {
+            if (lastWords.Count == markovDegree)
+            {
+                var determiningStateId = _determiningStateRepository.GetDeterminingStateByLastWord(lastWords);
+                _determinedWordRepository.AddDeterminedWord(word, determiningStateId);
+            }
+        }
     }
 }
