893fdbeca39e72bdc190bf8a53b829845738a73d
JulienBara <bara.julien@yahoo.fr>
2020-05-24 15:53:23 +0200

    move Polling app and install bot package

---
+++ Apps/AnaCSharpPolling/Program.cs
@@ -0,0 +1,14 @@
+﻿using Telegram.Bot;
+
+namespace AnaCSharpPolling
+{
+    class Program
+    {
+        private static TelegramBotClient Bot;
+
+        static void Main(string[] args)
+        {
+            Bot = new TelegramBotClient("bot token");
+        }
+    }
+}
