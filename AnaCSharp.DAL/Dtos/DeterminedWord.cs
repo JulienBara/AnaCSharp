90c7240ef194dbaf1e3dbfbd09f1465a794ab30d
JulienBara <bara.julien@yahoo.fr>
2020-05-24 13:39:30 +0200

    clean

--- AnaCSharp.DAL/Dtos/DeterminedWord.cs
+++ AnaCSharp.DAL/Dtos/DeterminedWord.cs
@@ -2,13 +2,6 @@
 {
     public class DeterminedWord : IDeterminedWord
     {
-        public int DeterminedWordId { get; set; }
-
-        public int DeterminingStateId { get; set; }
-
-        public IDeterminingState DeterminingState { get; set; }
-
-        public int WordId { get; set; }
 
         public IWord Word { get; set; }
 
--- Dal/AnaCsharp.Dal.Interfaces/Dtos/IDeterminedWord.cs
+++ Dal/AnaCsharp.Dal.Interfaces/Dtos/IDeterminedWord.cs
@@ -2,14 +2,6 @@
 {
     public interface IDeterminedWord
     {
-        int DeterminedWordId { get; set; }
-
-        int DeterminingStateId { get; set; }
-
-        IDeterminingState DeterminingState { get; set; }
-
-        int WordId { get; set; }
-
         IWord Word { get; set; }
 
         int Number { get; set; }
