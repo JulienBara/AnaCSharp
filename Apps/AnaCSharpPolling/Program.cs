fa55f4304ee0221c6dcc4b167a75ced093ea571f
Julien Bara <bara.julien@yahoo.fr>
2021-12-04 10:40:47 +0100

    update telegram version package

--- Apps/AnaCSharpPolling/Program.cs
+++ Apps/AnaCSharpPolling/Program.cs
@@ -2,15 +2,18 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
-using Telegram.Bot.Args;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
 namespace AnaCSharpPolling
 {
     class Program
     {
-        private static TelegramBotClient Bot;
         private static bool muted = false;
 
         static void Main(string[] args)
@@ -21,44 +24,74 @@ namespace AnaCSharpPolling
 
             IConfiguration config = builder.Build();
 
-            Bot = new TelegramBotClient(config["botToken"]);
+            var botClient = new TelegramBotClient(config["botToken"]);
 
-            var me = Bot.GetMeAsync().Result;
+            using var cts = new CancellationTokenSource();
 
-            Bot.OnMessage += BotOnMessageReceived;
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { } // receive all update types
+            };
+            botClient.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken: cts.Token);
 
-            Bot.StartReceiving(Array.Empty<UpdateType>());
+            var me = botClient.GetMeAsync().Result;
+
             Console.WriteLine($"Start listening for @{me.Username}");
             Console.ReadLine();
-            Bot.StopReceiving();
+
+            // Send cancellation request to stop bot
+            cts.Cancel();
         }
 
-        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        private static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            var message = messageEventArgs.Message;
+            // Only process Message updates: https://core.telegram.org/bots/api#message
+            if (update.Type != UpdateType.Message) return;
+            // Only process text messages
+            if (update.Message!.Type != MessageType.Text) return;
 
-            if (message == null || message.Type != MessageType.Text) return;
+            var chatId = update.Message.Chat.Id;
+            var messageId = update.Message.MessageId;
+            var messageText = update.Message.Text;
 
-            switch (message.Text.Split(' ').First())
+            switch (messageText.Split(' ').First())
             {
                 // Mute
                 case "/mute":
                     muted = true;
-                    await Bot.SendTextMessageAsync(message.Chat.Id, "Ana muted");
+                    await botClient.SendTextMessageAsync(chatId, "Ana muted");
                     break;
                 // Unmute
                 case "/unmute":
                     muted = false;
-                    await Bot.SendTextMessageAsync(message.Chat.Id, "Ana unmuted");
+                    await botClient.SendTextMessageAsync(chatId, "Ana unmuted");
                     break;
                 // Compute message
                 default:
                     if (muted)
                         break;
                     var answer = "pouet"; // await _anaService.GenerateAnswerAsync(message.Text);
-                    await Bot.SendTextMessageAsync(message.Chat.Id, answer, replyToMessageId: message.MessageId);
+                    await botClient.SendTextMessageAsync(chatId, answer, cancellationToken: cancellationToken, replyToMessageId: messageId);
                     break;
             }
+        }
+
+        private static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
         }
     }
 }
