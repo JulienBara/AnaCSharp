fcaab2b7d9fda2369e2d437b721b0c7744d5ef1f
Julien Bara <bara.julien@yahoo.fr>
2021-12-04 11:24:21 +0100

    properly host the bot to ease DI

---
+++ Apps/AnaCSharpPolling/Worker.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+
+namespace AnaCSharpPolling
+{
+    public class Worker : BackgroundService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(
+            IConfiguration configuration,
+            ILogger<Worker> logger
+            )
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var botClient = new TelegramBotClient(_configuration["botToken"]);
+
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { } // receive all update types
+            };
+            botClient.StartReceiving(
+                Handlers.HandleUpdateAsync,
+                Handlers.HandleErrorAsync,
+                receiverOptions,
+                cancellationToken: stoppingToken);
+
+            var me = await botClient.GetMeAsync();
+
+            _logger.Log(LogLevel.Information, $"Start listening for @{me.Username}");
+        }
+    }
+}
